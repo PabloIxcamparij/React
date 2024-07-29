fa8bab8a2b1796636224298c376cdc5302d81317
PabloIxcamparij <j.pablo.sorto@gmail.com>
2024-07-29 11:20:41 -0600

    Creacion de dos archivos .ts para la base del proyecto
    se añade el .gitignore y depedencias para trabajar con ts y comprobar cambios

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { sumar } from "./server"
+
+console.log("desde index.ts")
+
+sumar()
+
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+
+
+export function sumar() {
+    console.log("desde server.ts")
+
+    const suma  = 3 + 2
+
+    console.log(suma)
+
+}
